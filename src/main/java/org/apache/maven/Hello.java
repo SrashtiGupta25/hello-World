0f4c48dc95a880c33158b84aa0e8d8e3abd84841
pnikam <pnikam@ssni.com>
2019-02-06 12:28:23 +0530

    Hy, first task!

---
+++ src/main/java/org/apache/maven/Hello.java
@@ -0,0 +1,13 @@
+package org.apache.maven;
+//import com.google.gson.Gson;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+
+	}
+
+}
